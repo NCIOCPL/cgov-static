b716868af4fc2df4a0b6e77897bb1da50d9fb595
James Frank <james.frank@publicissapient.com>
2023-12-11 13:07:42 -0500

    (#16) Remove old sites
    Sites are either outdates/non-existent or get analytics via launch.
    
    Closes #16

--- public/WebAnalytics/wa_cssi_pre.js
+++ public/WebAnalytics/wa_cssi_pre.js
@@ -7,8 +7,7 @@ var wa_channel = '';
 var wa_search_function_name = '';
 var wa_production_url_match = '';
 var currentHostname = location.hostname.toLowerCase();
-var wa_linkInternalFilters = 'javascript:,' + currentHostname + ',';
-wa_linkInternalFilters += "itcr.cancer.gov";
+var wa_linkInternalFilters = 'javascript:,' + currentHostname;
 var page_URL = document.URL;
 var wa_p30 = '';
 var wa_p44 = '';
@@ -18,47 +17,6 @@ if (page_URL.indexOf('cssi.cancer.gov') != -1) {
     wa_channel = 'CSSI - OD';
     wa_search_function_name = 'CSSI - OD - Search';
     wa_production_url_match = 'cssi.cancer.gov';
-}
-else if (page_URL.indexOf('imat.cancer.gov') != -1) {
-    wa_channel = 'CSSI - IMAT';
-    wa_search_function_name = 'CSSI - IMAT - Search';
-    wa_production_url_match = 'imat.cancer.gov';
-}
-else if (page_URL.indexOf('nano.cancer.gov') != -1) {
-    wa_channel = 'CSSI - Nano';
-    wa_search_function_name = 'CSSI - Nano - Search';
-    wa_production_url_match = 'nano.cancer.gov';
-}
-else if (page_URL.indexOf('ncl.cancer.gov') != -1) {
-    wa_channel = 'CSSI - NCL';
-    wa_search_function_name = 'CSSI - NCL - Search';
-    wa_production_url_match = 'ncl.cancer.gov';
-}
-else if (page_URL.indexOf('physics.cancer.gov') != -1) {
-    wa_channel = 'CSSI - Physics';
-    wa_search_function_name = 'CSSI - Physics - Search';
-    wa_production_url_match = 'physics.cancer.gov';
-}
-else if (page_URL.indexOf('proteomics.cancer.gov') != -1) {
-    wa_channel = 'CSSI - Proteomics';
-    wa_search_function_name = 'CSSI - Proteomics - Search';
-    wa_production_url_match = 'proteomics.cancer.gov';
-}
-else if (page_URL.indexOf('fundedresearch.cancer.gov') != -1) {
-    wa_channel = 'CSSI - Funded Research';
-    wa_search_function_name = 'CSSI - Funded Research- Search';
-    wa_production_url_match = 'fundedresearch.cancer.gov';
-}
-else if (page_URL.indexOf('assays.cancer.gov') != -1) { 
-    wa_channel = 'CSSI - Assays';
-    wa_search_function_name = 'CSSI - Assays- Search';
-    wa_production_url_match = 'assays.cancer.gov';
-}
-else if (page_URL.indexOf('itcr.cancer.gov') != -1) {	
-    wa_channel = 'CSSI - ITCR';
-    wa_search_function_name = 'CSSI - ITCR - Search';
-    wa_production_url_match = 'itcr.cancer.gov';
-    wa_is_production_report_suite = true;
 }
 else {
     wa_channel = 'CSSI dev';
